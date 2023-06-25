422428b08cf42543cbcb73a25e74fee6c7d1791d
AymenHattab <moktatafet.tube@gmail.com>
2023-06-25 16:18:58 +0100

    yes

--- db.configue.js
+++ db.configue.js
@@ -3,8 +3,9 @@ import sql from "mysql2"
 
 //configuration base de donnee 
 export const db= sql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"hgedb04"
+    host:"bbpkrs9mijn0sn7kusbc-mysql.services.clever-cloud.com",
+    user:"umb5il8bn2h7e0zp",
+    password:"3UznjQLEXmmkwLcK0OVf",
+    database:"bbpkrs9mijn0sn7kusbc",
+    port : "3306"
 })
